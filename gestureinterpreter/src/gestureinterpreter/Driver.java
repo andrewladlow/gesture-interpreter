42c76fc5a209ac4295e791ba900a876c4895970b
Andrew Ladlow <andrew.ladlow@gmail.com>
2015-08-10 16:16:00 +0100

    Include leapmotion x64 libs

--- gestureinterpreter/src/gestureinterpreter/Driver.java
+++ gestureinterpreter/src/gestureinterpreter/Driver.java
@@ -1,7 +1,31 @@
 package gestureinterpreter;
 
+import com.leapmotion.leap.*;
+
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		SampleListener listener = new SampleListener();
+		Controller controller = new Controller();
+		
+		controller.addListener(listener);
+		
+		System.out.println("Press Enter to quit");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		controller.removeListener(listener);
+	}
+}
+
+class SampleListener extends Listener {
+	public void onConnect(Controller controller) {
+		System.out.println("Connected");
+	}
+	
+	public void onFrame(Controller controller) {
+		System.out.println("Frame available");
 	}
 }
