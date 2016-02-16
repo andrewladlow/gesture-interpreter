c06329b6a46897e6f9e5876e2ec0c75e944346e4
Andrew Ladlow <andrew.ladlow@gmail.com>
2016-02-16 01:25:21 +0000

    Updated report

--- gestureinterpreter/src/gestureinterpreter/LeapListener.java
+++ gestureinterpreter/src/gestureinterpreter/LeapListener.java
@@ -1,5 +1,6 @@
 package gestureinterpreter;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 
 import com.leapmotion.leap.Controller;
@@ -15,7 +16,8 @@ public class LeapListener extends Listener {
 	private Group handGroup;
 	private HashMap<Integer, HandFX> hands;
 	private Menu app;
-	
+    //private DecimalFormat df = new DecimalFormat("###.##");
+    
 	public LeapListener(HashMap<Integer, HandFX> hands, Menu app) {
 		this.hands = hands;
 		this.app = app;
@@ -33,13 +35,8 @@ public class LeapListener extends Listener {
     
 	public void onFrame(Controller controller) {
 		Frame frame = controller.frame();
-	
+    	//long time1 = System.nanoTime();
 		Platform.runLater(() -> {
-			// refresh frame if there are less hands in this frame than in the last one
-			if (frame.hands().count() < controller.frame(1).hands().count()) {
-				//hands.clear();
-				handGroup.getChildren().clear();
-			}
 			if (!frame.hands().isEmpty()) {
 				for (Hand leapHand : frame.hands()) {
 					int handId = leapHand.id();
@@ -52,8 +49,19 @@ public class LeapListener extends Listener {
 					}							
 					if (hand != null) {
 						hand.update(frame.hand(leapHand.id()));
+			            //long time2 = System.nanoTime();
+						//String timeTaken = df.format(Math.round(time2 - time1) / 1e6);
+			            //System.out.println("Time taken: " + timeTaken + " ms");
 					}
 				}
+			}
+			// refresh frame if there are less hands in this frame than in the last one
+			if (frame.hands().count() < controller.frame(1).hands().count()) {
+				for (Hand leapHand : frame.hands()) {
+					// first remove from storage hashmap
+					hands.remove(leapHand.id());
+				}
+				handGroup.getChildren().clear();
 			}
 		});
 	}
