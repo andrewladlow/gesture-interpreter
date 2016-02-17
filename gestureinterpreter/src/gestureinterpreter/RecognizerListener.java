db972d2ab9273472b11d9cb68da7d44d045c9793
Andrew Ladlow <andrew.ladlow@gmail.com>
2016-02-17 02:17:27 +0000

    Updated report

--- gestureinterpreter/src/gestureinterpreter/RecognizerListener.java
+++ gestureinterpreter/src/gestureinterpreter/RecognizerListener.java
@@ -96,7 +96,7 @@ public class RecognizerListener extends Listener {
 		Frame frame = controller.frame();
 		if (!frame.hands().isEmpty()) {					
 			// enforce delay between recognitions
-			if (System.currentTimeMillis() - timeRecognized > 250) {	        
+			//if (System.currentTimeMillis() - timeRecognized > 250) {	        
 		        if (validFrame(frame, minGestureVelocity, maxPoseVelocity)) {	            	          
 		            if (state == State.IDLE) {
 		            	gestureFrameCount = 0;
@@ -130,7 +130,7 @@ public class RecognizerListener extends Listener {
 		            }		            
 	                validPose = false;
 		        }
-			}
+			//}
 		} 
 	}
 
